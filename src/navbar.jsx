b869176f7db8b712de733ca14881e8c2653e60d2
Mir Abbas Hussain <141949223+Mir-pes@users.noreply.github.com>
2025-03-24 16:37:29 +0530

    Update navbar.jsx

--- src/navbar.jsx
+++ src/navbar.jsx
@@ -18,7 +18,7 @@ function Navbar() {
             <div className="container">
                 {/* Contact Info Section */}
                 <div className="contact-info">
-                    <span className="address">Khalid Bin Al Waleed Bldg | P.O.Box – 111923 | Dubai | UAE</span>
+                    <span className="address">Khalid Bin Al Waleed Building | P.O.Box – 111923 | Dubai | UAE</span>
                     <span className="phone">+971-4-3272129</span>
                     <span className="emailid">relocation@ipanacllc.com</span>
                 </div>
