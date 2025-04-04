9ef977b3e071dfb267ec98db18ebbc48482ffc06
Mir Abbas Hussain <141949223+Mir-pes@users.noreply.github.com>
2025-04-04 23:32:29 +0530

    Update quickquote.jsx

--- src/quickquote.jsx
+++ src/quickquote.jsx
@@ -62,7 +62,7 @@ function Quote() {
                                 Furniture
                             </div>
 
-                            <div className="checkbox-group2" style={{marginTop:"8px",marginLeft:"150px"}}>
+                            <div className="checkbox-group2" style={{marginTop:"8px",marginLeft:"158px"}}>
                                 <input type="checkbox" name="items" value="varying"/>
                                 Appliances
                             </div>
