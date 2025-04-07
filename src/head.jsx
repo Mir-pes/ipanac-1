f7dfe7737ab5ff5474e2a1be371fb4d5b2cf94be
Mir Abbas Hussain <141949223+Mir-pes@users.noreply.github.com>
2025-04-07 16:41:48 +0530

    Update head.jsx

--- src/head.jsx
+++ src/head.jsx
@@ -8,7 +8,7 @@ function Head(){
 
       <div className="hero-container">
       <h1 className="hero-text">
-        <b>At IPANAC, we specialize in making your relocation experience stress-free,<br></br> 
+        <b>At iPANAC, we specialize in making your relocation experience stress-free,<br></br> 
         smooth, and efficient.</b>
       </h1>
       <p className = "para">Whether you're moving across the globe or locally within the GCC, we are here to provide personalized services tailored to your unique needs. From packing to settling in, we handle all aspects of your move with professionalism, care, and expertise.
