b237577cf8b4762eaf15033e0f5057f1c49c2e89
Mir Abbas Hussain <141949223+Mir-pes@users.noreply.github.com>
2025-05-24 20:00:54 +0530

    Update featuresection.jsx

--- src/featuresection.jsx
+++ src/featuresection.jsx
@@ -4,12 +4,12 @@ import "./FeatureSection.css";
 function FeatureSection() {
   return (
     <div className="feature-section">
-      <div className="feature-container">
-        <span className="service-badge">
+      <div className="feature-container" data-aos="fade-up">
+        <span className="service-badge" data-aos="zoom-in" data-aos-delay="100">
           Our Services
         </span>
         
-        <h2 className="heading">
+        <h2 className="heading" data-aos="fade-up" data-aos-delay="200">
           Easily transit from 
           <span className="gradient-text">
             {" "}one Location to another
@@ -18,13 +18,14 @@ function FeatureSection() {
         
         <div className="features-grid">
           {features.map((feature, index) => (
-            <div key={index} className="feature-item">
+            <div key={index} className="feature-item" data-aos="fade-up" data-aos-delay={300 + index * 100}>
+              <h5 className="feature-title">{feature.text}</h5>
               <div className="flex">
                 <div className="icon-container">
-                  {feature.icon}
+                  <img src={feature.image} alt={feature.text} className="feature-image" />
                 </div>
               </div>
-              <h5 className="feature-title">{feature.text}</h5>
+              
               <p className="feature-description">{feature.description}</p>
             </div>
           ))}
